4a775b52e0736b488ca95882bbbf53b91f56a446
“alessioinnocenti” <alessioinnocenti@yahoo.it>
2022-03-08 11:14:33 +0000

    modified preproc A2

--- chain_env.sh
+++ chain_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # MIT_HOME is your MITgcm_BFM_chain directory, the root of the git repository
-export MIT_HOME=/home/innocenti/MITgcm_BFM/NWMed
+export MIT_HOME=/home/itai/MITgcm_BFM/NWMed
 # your machine name
-export MIT_HOSTNAME=hpc-fe1
+export MIT_HOSTNAME=atos
 # your work-launch (e.g., "scratch" area) directory in a parallel filesystem
-export MIT_WORKDIR=/home/innocenti/MITgcm_BFM/WORK_NWMed
+export MIT_WORKDIR=/ec/res4/scratch/itai/MITgcm_BFM/WORK_NWMed
 export MIT_VERSION_NUMBER=1
 export MIT_STAGE=devel
 alias mitcd="cd $MIT_HOME ; pwd"
